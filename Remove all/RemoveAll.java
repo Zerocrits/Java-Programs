47bc0227e6a0e0670f57d1d0e605fca264b15485
Zerocrits <12242864@WH268-13.wayneschools.net>
2015-11-23 10:26:05 -0500

    Added remove all program and added for each loop pdf

---
+++ Remove all/RemoveAll.java
@@ -0,0 +1,28 @@
+//ArrayList remove() exercise
+
+import java.util.ArrayList;
+
+public class RemoveAll
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> ray;
+		ray = new ArrayList<String>();
+
+		ray.add("all");
+		ray.add("all");
+		ray.add("fun");
+		ray.add("dog");
+		ray.add("bat");
+		ray.add("cat");
+		ray.add("all");
+		ray.add("dog");
+		ray.add("all");
+		ray.add("all");
+		System.out.println(ray);
+		System.out.println(ray.size());
+
+		//add in a loop to remove all occurrences of all
+
+	}
+}
