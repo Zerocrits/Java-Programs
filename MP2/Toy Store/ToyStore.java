437e7196b52a82300cf68578213320d631c3eda3
Zerocrits <12242864@WH268-13.wayneschools.net>
2015-12-23 10:25:06 -0500

    asda

--- MP2/Toy Store/ToyStore.java
+++ MP2/Toy Store/ToyStore.java
@@ -14,7 +14,7 @@ public class ToyStore
 
 	public ToyStore()
 	{
-		getThatToy("");
+		toyList = new ArrayList<Toy>();
 	}
 
 	/** Load toys takes a String such as "Clue Clue Clue Sorry Sorry" as a parameter
@@ -25,29 +25,13 @@ public class ToyStore
 	public void loadToys( String toys )
 	{
 		String[] toy = toys.split(" ");
-		for(int i = 0; i < toy.length(); i++)
+		for(int i = 0; i < toy.length; i++)
 		{
-			if(getThatToy(toy.get(i).getName() != null)
-				toyList.setCount
+			if(getThatToy(toy[i]) != null)
+				getThatToy(toy[i]).setCount(getThatToy(toy[i]).getCount() + 1);
+			else
+				toyList.add(new Toy(toy[i]));
 		}
-		/*for(int i = 0; i < toyList.size(); i++)
-		{
-			temp = 0;
-
-			for(int j = i+1; j < toyList.size(); j++)
-			{
-				if(getThatToy(toyList.get(i).getName()) == null)
-				{
-					temp++;
-					toyList.remove(j);
-				}
-			}
-			toyList.get(i).setName(toyList.get(i).getName());
-			toyList.remove(i);
-			toyList.get(i).setCount(temp);
-			toyList.get(i).getName();
-			toyList.get(i).getCount();
-		}*/
 	}
 
 	/** Searches toyList for a toy with the same name as the parameter nm
@@ -55,33 +39,27 @@ public class ToyStore
 	*/
 	public Toy getThatToy( String nm )
 	{
-		toyList = new ArrayList();
-
-		for(Toy t:toyList)
-			if(t.getName().equals(nm))
-				return t;
+		for(int i = 0; i < toyList.size(); i++)
+		{
+			if(toyList.get(i).getName().equals(nm))
+				return toyList.get(i);
+		}
 		return null;
 	}
 
 	/** @return the name of the toy that occurs the most frequently in toyList */
 	public String getMostFrequentToy()
 	{
-		String name = "";
-		int temp = 0;
-		int temp1 = 0;
-		for(int i = 0; i < toyList.size(); i++)
-		{
-			for(int j = 0; j < toyList.size(); j++)
-				if(toyList.get(i) == toyList.get(j))
-					temp++;
-					temp1 = i;
-		}
-		name = toyList.get(temp1).getName();
-		return name;
+		Toy constant = toyList.get(0);
+		for(int i = 1; i < toyList.size(); i++)
+			if(toyList.get(i).getCount() > constant.getCount())
+				constant = toyList.get(i);
+		return "Max is: " + constant.toString();
 	}
 
 	public void sortToysByCount()
 	{
+
 	}
 
 	public String toString()
@@ -91,14 +69,8 @@ public class ToyStore
 }
 
 /*
-		for(int i = 0; i < toyList.size(); i++)
-		{
-			for(int j = 0; j < toyList.size(); j++)
-			{
-				if(toyList.get(i).getName() == toyList.get(j).getName())
-					temp++;
-			}
-			toyList.get(i).setCount(temp);
-			toyList.get(i).getCount();
-			toyList.get(i).getName();
+
+
+
+
 */
--- MP2/Toy Store/ToyStoreRunner.java
+++ MP2/Toy Store/ToyStoreRunner.java
@@ -14,14 +14,19 @@ public class ToyStoreRunner
 		System.out.println(toy);
 		toy.loadToys("sorry bat sorry sorry sorry train train teddy teddy ball ball");
 		System.out.println(toy);
+		System.out.println(toy.getMostFrequentToy());
 		//System.out.println(toy.sortToysByCount);
 
+		toy = new ToyStore();
 		toy.loadToys("yahtzee, monopoly, candyland, monopoly, yahtzee, candyland");
-		System.out.println(toy);
+		System.out.println("\n" + toy);
+		System.out.println(toy.getMostFrequentToy());
 		//System.out.println(sortToysByCount);
 
+		toy = new ToyStore();
 		toy.loadToys("operation, operation, boggle, scrabble, clue, life, clue, clue");
-		System.out.println(toy);
+		System.out.println("\n" + toy);
+		System.out.println(toy.getMostFrequentToy());
 		//System.out.println(sortToysByCount);
 	}
 }
