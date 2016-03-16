dfe79e1f3113b164876bc732872a74c2eaedfc16
Zerocrits <12242864@wayneschools.net>
2016-03-16 10:25:04 -0400

    asda

--- MP3/Animal/PetGUI.java
+++ MP3/Animal/PetGUI.java
@@ -8,27 +8,27 @@ import javax.swing.*;
 
 public class PetGUI extends JFrame
 {
-	private int APPLET_WIDTH = 300, APPLET_HEIGHT = 470;
-	private JLabel lblAllSpeak, lblTitle, lblDog, lblLoudDog, lblCat;
+	private int APPLET_WIDTH = 350, APPLET_HEIGHT = 470;
+	private JLabel lblTitle, lblDog, lblLoudDog, lblCat, lblName;
 	private ImageIcon icoDog, icoLoudDog, icoCat;
-	private JButton btnDog, btnLoudDog, btnCat;
+	private JButton btnDog, btnLoudDog, btnCat, btnAllSpeak;
+	private JTextField txtAllSpeak, txtInput;
 	//private SlotMachine slot;
 	private ButtonListener listener;
 
-	public SlotGUI()
+	public PetGUI()
 	{
 		Container cp = getContentPane();
-		cp.setBackground(Color.cyan);
 		cp.setLayout(new FlowLayout());
 
-		coins = 0;
-
-		btnDog = new JButton ("     Dog     ");
-		btnDog.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
-		btnLoudDog = new JButton ("   Loud Dog  ");
-		btnLoudDog.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
-		btnCat = new JButton ("     Cat     ");
-		btnCat.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+		btnDog = new JButton ("   Dog   ");
+		btnDog.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		btnLoudDog = new JButton ("  Loud Dog ");
+		btnLoudDog.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		btnAllSpeak = new JButton ("        All Speak        ");
+		btnAllSpeak.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		btnCat = new JButton ("   Cat   ");
+		btnCat.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
 		btnDog.addActionListener( new ButtonListener());
 		btnLoudDog.addActionListener( new ButtonListener());
 		btnCat.addActionListener( new ButtonListener());
@@ -37,36 +37,34 @@ public class PetGUI extends JFrame
 		icoLoudDog = new ImageIcon("lblLoudDog.PNG");
 		icoCat = new ImageIcon("lblCat.PNG");
 
-		lblAllSpeak = new JLabel();
-		lblTitle = new JLabel("    Slot Machine    ");
+		lblDog = new JLabel();
+		lblLoudDog = new JLabel();
+		lblCat = new JLabel();
+		lblName = new JLabel();
+
+		txtInput = new JTextField(20);
+		txtAllSpeak = new JTextField(30);
+		txtAllSpeak.setEditable(false);
+		lblTitle = new JLabel("       Pet GUI       ");
 		lblTitle.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
 		listener = new ButtonListener();
 
-		lblDog.setIcon(icoDice);
-		lblCherry.setIcon(icoCherry);
-		lblSeven.setIcon(icoSeven);
-		lblBar.setIcon(icoBar);
-		lblBlank1.setIcon(icoBlank1);
-		lblBlank2.setIcon(icoBlank2);
-		lblBlank3.setIcon(icoBlank3);
+		lblDog.setIcon(icoDog);
+		lblLoudDog.setIcon(icoLoudDog);
+		lblCat.setIcon(icoCat);
+		lblName.setText("Name: ");
 
 		cp.add(lblTitle);
-		cp.add(lblBlank1);
-		cp.add(lblBlank2);
-		cp.add(lblBlank3);
-		cp.add(btnPull);
-		cp.add(lblCoins);
-		cp.add(lblBlank);
-		cp.add(lblInfo1);
-		cp.add(lblInfo2);
-		cp.add(lblInfo3);
-		cp.add(lblInfo4);
-
-		lblBlank.setText("                     ");
-		lblInfo1.setText("Bar = $100");
-		lblInfo2.setText("Cherry = $500");
-		lblInfo3.setText("Dice = $1,000");
-		lblInfo4.setText("Seven = $10,000");
+		cp.add(lblDog);
+		cp.add(lblLoudDog);
+		cp.add(lblCat);
+		cp.add(lblName);
+		cp.add(txtInput);
+		cp.add(btnDog);
+		cp.add(btnLoudDog);
+		cp.add(btnCat);
+		cp.add(btnAllSpeak);
+		cp.add(txtAllSpeak);
 
 		setSize(APPLET_WIDTH, APPLET_HEIGHT);
 		setVisible(true);
@@ -74,66 +72,33 @@ public class PetGUI extends JFrame
 
 	public static void main(String[] args)
 	{
-		SlotGUI gui = new SlotGUI();
-	}
-	public String getDelay()
-	{
-		String word = "";
-		while(i <= 4)
-		{
-			Thread.sleep(250);
-			switch(i)
-			{
-				case 1: word = "Bar"; break;
-				case 2: word = "Cherry"; break;
-				case 3: word = "Dice"; break;
-				case 4: word = "7"; break;
-			}
-			i++;
-			return word;
-		}
-		return word;
+		PetGUI gui = new PetGUI();
 	}
 
 	// private inner class for handling button events
     // action listener requires actionPerformed method
 	private class ButtonListener implements ActionListener
     {
-        public void actionPerformed(ActionEvent event)  catch InterruptedException
+        public void actionPerformed(ActionEvent event)
         {
 			// find out what object was pressed:
 			Object source = new Object();
 			source = event.getSource();
 
-			if(source == btnPull)
+			if(source == btnDog)
 			{
-				word1 = getDelay();
-				word2 = slot.spin2();
-				word3 = slot.spin3();
-				switch(word1)
-				{
-					case "Bar": lblBlank1.setIcon(icoBar); break;
-					case "Cherry": lblBlank1.setIcon(icoCherry); break;
-					case "Dice": lblBlank1.setIcon(icoDice); break;
-					case "7": lblBlank1.setIcon(icoSeven); break;
-				}
-				switch(word2)
-				{
-					case "Bar": lblBlank2.setIcon(icoBar); break;
-					case "Cherry": lblBlank2.setIcon(icoCherry); break;
-					case "Dice": lblBlank2.setIcon(icoDice); break;
-					case "7": lblBlank2.setIcon(icoSeven); break;
-				}
-				switch(word3)
-				{
-					case "Bar": lblBlank3.setIcon(icoBar); break;
-					case "Cherry": lblBlank3.setIcon(icoCherry); break;
-					case "Dice": lblBlank3.setIcon(icoDice); break;
-					case "7": lblBlank3.setIcon(icoSeven); break;
-				}
-				coins = coins + slot.getPayout();
-				lblCoins.setText("Coins: $" + coins);
-			}//if
+				txtAllSpeak.setText("test");
+			}
+
+			else if(source == btnLoudDog)
+			{
+
+			}
+
+			else
+			{
+
+			}
 		}//actionPerformed()
 	}//inner class
 }
---
+++ MP3/Brawl/Character.java
@@ -0,0 +1,66 @@
+public abstract class Character
+{
+	private int level;
+	private int health;
+
+	/** Default Constructor
+	*	Sets the level and health to reasonable defaults
+	*/
+	public Character()
+	{
+
+	}
+
+	/** Initialization Constructor
+	*	Sets level and health based on the parameters l and h
+	*/
+	public Character(int l, int h)
+	{
+
+	}
+
+	public int getHealth()
+	{
+
+	}
+
+	public void setHealth(int value)
+	{
+
+	}
+
+	public int getLevel()
+	{
+
+	}
+
+	/** @return true if health is still greater than 0, otherwise false */
+	public boolean isNotDefeated()
+	{
+
+	}
+
+	/**	@return a String that shows "Damage: " and the amount of damage this creature caused to Character c
+	*	Character c has their health decreased by a random amount from 1 to the level of this Character
+	*/
+	public String attacks(Character c)
+	{
+
+	}
+
+	/**	reduces health for this Character by amount */
+	public void decreaseHealth(int amount)
+	{
+
+	}
+
+	/** @return a random attack from a list appropriate to the particular Character */
+	public abstract String getNameOfAttack();
+
+	/**	@return a String with an ASCII picture of the particular Character */
+	public abstract String displayCharacter();
+
+	/** @return the level and health of this particular Character */
+	public abstract String toString();
+
+}
